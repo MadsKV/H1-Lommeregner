a5bf328d13bdebaf77f115a601d46126f49acdbe
Jack-Andersen <62236370+Jack-Andersen@users.noreply.github.com>
2020-08-04 11:23:39 +0200

    Divide and multiply created
    Methods

--- Methods.cs
+++ Methods.cs
@@ -6,7 +6,23 @@ namespace Calculator_mehtods
     {
         public static int Add(int x, int y)
         {
+         
+        }
 
+        public static double Divide(double x, double y)
+        {
+            double number1 = x;
+            double number2 = y;
+            double answer = number1 / number2;
+            return answer;
+        }
+
+        public static double Multiply(double x, double y)
+        {
+            double number1 = x;
+            double number2 = y;
+            double answer = number1 * number2;
+            return answer;
         }
     }
 }
